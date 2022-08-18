30ddc6eb22ca044198239954321ff66abcbf3e68
Amirali Vakili <amirv1382@gmail.com>
2022-08-18 13:51:02 +0430

    board def setupped

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ window.title("XO Game")
 
 def board():
     board_frame = tk.Frame(window)
-
+    board_frame.grid(row=0)
+    label_player_one = tk.Label(board_frame, text="player1", font=("Aviny", 16), padx=10)
 
 window.mainloop()
