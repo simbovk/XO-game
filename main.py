ff7eb56571f80936b80226b4e112fe5c22ba1ecd
Amirali Vakili <amirv1382@gmail.com>
2022-08-18 13:59:32 +0430

    point frame stupe

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ window.title("XO Game")
 def board():
     board_frame = tk.Frame(window)
     board_frame.grid(row=0)
-    label_player_one = tk.Label(board_frame, text="player 1", font=("italic", 16), padx=10)
-    label_player_two = tk.Label(board_frame, text="player 2", font=("italic", 16), padx=10)
-    label_player_one.grid(row=0, column=0)
-    label_player_two.grid(row=0, column=2)
+    player_one_label = tk.Label(board_frame,text="player 1", font=("italic", 16), padx=10)
+    player_two_label = tk.Label(board_frame,text="player 2", font=("italic", 16), padx=10)
+    player_one_label.grid(row=0, column=0)
+    player_two_label.grid(row=0, column=2)
+
+    point_frame = tk.Frame(window)
+
 
 board()
 window.mainloop()
