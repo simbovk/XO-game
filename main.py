e1e84ff6fe2b2003b75bd9ca642c4b2f8f776153
Amirali Vakili <amirv1382@gmail.com>
2022-08-18 14:03:00 +0430

    player point label updated

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ window.title("XO Game")
 def board():
     board_frame = tk.Frame(window)
     board_frame.grid(row=0)
-    player_one_label = tk.Label(board_frame,text="player 1", font=("italic", 16), padx=10)
-    player_two_label = tk.Label(board_frame,text="player 2", font=("italic", 16), padx=10)
+    player_one_label = tk.Label(board_frame, text="player 1", font=("Italic", 16), padx=10)
+    player_two_label = tk.Label(board_frame, text="player 2", font=("Italic", 16), padx=10)
     player_one_label.grid(row=0, column=0)
     player_two_label.grid(row=0, column=2)
 
     point_frame = tk.Frame(window)
-
-
+    point_frame.grid(row=1)
+    player_one_point = tk.Label(point_frame, text="0", font=("Italic", 16), padx=20)
+    player_two_point = tk.Label(point_frame, text="0", font=("Italic", 16), padx=20)
 board()
 window.mainloop()
