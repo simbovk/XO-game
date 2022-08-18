afecdc45c6c576d839c1625e9f98a7d7152315e5
Amirali Vakili <amirv1382@gmail.com>
2022-08-18 13:51:33 +0430

    players label setupped

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ def board():
     board_frame = tk.Frame(window)
     board_frame.grid(row=0)
     label_player_one = tk.Label(board_frame, text="player1", font=("Aviny", 16), padx=10)
+    label_player_two = tk.Label(board_frame, text="player2", font=("Aviny", 16), padx=10)
 
 window.mainloop()
